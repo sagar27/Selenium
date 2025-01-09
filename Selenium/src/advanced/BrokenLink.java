99eee79aa9d6434728c44fff7a2ecd027695ba31
sagar <sagar@192.168.1.36>
2025-01-09 15:36:38 +0530

    if it works, it works

--- Selenium/src/advanced/BrokenLink.java
+++ Selenium/src/advanced/BrokenLink.java
@@ -11,7 +11,6 @@ import org.openqa.selenium.chrome.ChromeDriver;
 public class BrokenLink {
 	
 	public static void main(String args[]) throws IOException {
-	// TODO Auto-generated method stub
 		
 		WebDriver driver = new ChromeDriver();
 		driver.manage().window().maximize();
