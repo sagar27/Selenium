eef3ba138c9f8fbfb4737fb97cc9eff67ee77de1
sagar <sagar@192.168.1.37>
2025-01-16 20:06:58 +0530

    c

--- Selenium/src/advanced/Screenshot.java
+++ Selenium/src/advanced/Screenshot.java
@@ -13,7 +13,6 @@ import org.openqa.selenium.chrome.ChromeDriver;
 public class Screenshot {
 
 	public static void main(String[] args) throws IOException {
-		// TODO Auto-generated method stub
 		
 		WebDriver driver = new ChromeDriver();
 		driver.get("https://www.google.com");
